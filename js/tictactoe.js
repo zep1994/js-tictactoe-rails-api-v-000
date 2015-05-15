700179c33701967093c900ef93c6fd2a199a05f8
Katie Hoffman <ktahoffman@gmail.com>
2015-05-15 13:07:21 -0400

    merge with master, change message function so that specs pass

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,26 +1,52 @@
 var turn = 0;
-var winningCombos = [[[0,0],[1,0],[2,0]], [[0,1],[1,1],[2,1]], [[0,2],[1,2],[2,2]], [[0,0],[1,1],[2,2]], [[0,0],[0,1],[0,2]], [[2,0],[2,1],[2,2]], [[1,0],[1,1],[1,2]], [[2,0],[1,1],[0,2]]];
-
+var winningCombos = [[[0,0],[1,0],[2,0]], [[0,1],[1,1],[2,1]], [[0,2],[1,2],[2,2]], [[0,0],[1,1],[2,2]], [[0,0],[0,1],[0,2]], [[2,0],[2,1],[2,2]], [[1,0],[1,1],[1,2]], [[2,0],[1,1],[0,2]]]
+var checkCells = function(ary) {
+  for(var i = 0; i < ary.length; i++) {
+    var winningCombo = ary[i];
+    var x = winningCombo[0];
+    var y = winningCombo[1];
+    var selector = $('[data-x="' + x + '"][data-y="' + y + '"]')
+    if( noCellMatch(selector)) {
+      return false;
+    }
+  }
+  return true;
+}
 var checkWinner = function() {
-  // your code here 
+  for(var i = 0; i < winningCombos.length; i++) {
+    if(checkCells(winningCombos[i]) == true) {
+      return true;
+    }
+  }
+  return false;
 }
 
 var player = function() {
-  // your code here
+  if(turn % 2 == 0) {
+    return "X";
+  }
+  else {
+    return "O";
+  }
+};
+var noCellMatch = function(element) {
+  return (element.html() != player())
 }
-
 var doTurn = function(event){
-  // your code here
+  updateState(event);
+  if(checkWinner()) {
+    message("Player " + player() + " Won!")
+  }
+  turn += 1;
 }
-
 var message = function(message) {
-  // your code here
+  $("#message").html(message);
 }
-
 var updateState = function(event) {
-  // your code here
+  $(event.target).html(player());
 }
-
 var attachListeners = function() {
-  // your code here
+  $("tbody").click(function(event) {
+    doTurn(event)
+  })
 }
