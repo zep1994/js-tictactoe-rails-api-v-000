35bc1275eda5e0ac977c85a9685c92414eadc1dc
Blake Johnson <blake41@gmail.com>
2016-02-17 11:51:42 -0500

    specs bleeding across tests

--- spec/tictactoe.spec.js
+++ spec/tictactoe.spec.js
@@ -232,6 +232,10 @@ describe('Tic Tac Toe Functionality', function() {
 
 
 describe('#integration tests of persistence', function() {
+  beforeEach(function(){
+    turn = 0;
+    currentGame = 0;
+  });
   it("if i click the getAllGames button it should send a get request to /games", function() {
     setFixtures('<body><table border="1" cellpadding="40"><tr><td data-x="0", data-y="0">X</td><td data-x="1", data-y="0"></td><td data-x="2", data-y="0"></td></tr><tr><td data-x="0", data-y="1">O</td><td data-x="1", data-y="1"></td><td data-x="2", data-y="1"></td></tr><tr><td data-x="0", data-y="2"></td><td data-x="1", data-y="2"></td><td data-x="2", data-y="2"></td></tr></table><div id="games"></div><div id="message"></div><button id="save">Save Game</button><button id="previous">Show Previous Games</button></body>');
     attachListeners()
