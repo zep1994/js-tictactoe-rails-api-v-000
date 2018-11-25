5e6732c25390e95b7fcb6f858148f925ef0a64e2
zep1994 <snaxbags@protonmail.com>
2018-11-24 19:53:31 -0600

    Done.

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'active_model_serializers', '0.10.6'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.2.5.1'
+gem 'rails', '4.2.8'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,150 @@
-// Code your JavaScript / jQuery solution here
+var turn = 0
+var currentGame = 0
+const WIN_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+$(function(){
+  attachListeners()
+})
+
+function player() {
+  if (turn % 2 === 0){
+    return "X"
+  } else {
+    return "O"
+  }
+}
+
+function updateState(state) {
+  var key = player()
+  $(state).text(key)
+}
+
+function setMessage(message) {
+  $('#message').text(message)
+}
+
+function checkWinner() {
+  let winning_combination = false;
+  let board = {}
+  const WIN_COMBOS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  $('td').text((index, box) => board[index] = box)
+
+  WIN_COMBOS.forEach(function(combo) {
+  if (board[combo[0]] !== "" && board[combo[0]] === board[combo[1]] && board[combo[1]] === board[combo[2]]) {
+    setMessage(`Player ${board[combo[0]]} Won!`)
+    return winning_combination = true;
+    }
+  })
+  return winning_combination
+}
+
+function doTurn(box) {
+  updateState(box)
+  turn++
+  if(checkWinner()) {
+    saveGame()
+    resetBoard()
+  } else if (turn === 9) {
+    setMessage("Tie game.")
+    saveGame()
+    resetBoard()
+  }
+}
+
+function resetBoard() {
+  $('td').empty()
+  turn = 0
+  currentGame = 0
+}
+
+function resetGame(gameID) {
+  document.getElementById('message').innerHTML = ''
+  const req = new XMLHttpRequest
+
+  req.overrideMimeType('application/json')
+  req.open('GET', `/games/${gameID}`)
+  req.onload = () => {
+    const data = JSON.parse(req.responseText).data
+    const id = data.id
+    const state = data.attributes.state
+    let index = 0
+
+    for (let y = 0; y < 3; y++) {
+      for (let x = 0; x < 3; x++) {
+        document.querySelector(`[data-x='${x}'][data-y='${y}']`).innerHTML = state[index]
+        index++
+      }
+    }
+    turn = state.join('').length
+    currentGame = id
+    if (!checkWinner() && turn === 9) {
+      setMessage('Tie game.')
+    }
+  }
+  req.send(null)
+}
+
+function saveGame() {
+  var state = []
+  var gameData;
+  $('td').text((index, square) => {
+    state.push(square)
+  })
+
+  gameData = { state: state }
+  if (currentGame) {
+      $.ajax({
+        type: 'PATCH',
+        url: `/games/${currentGame}`,
+        data: gameData
+      })
+  } else {
+    $.post('/games', gameData, function(game){
+      currentGame = game.data.id
+      $('#games').append(`<button id="gameid-${game.data.id}">${game.data.id}</button><br />`)
+      $('#gameid-' + game.data.id).on('click', () => resetGame(game.data.id))
+    })
+  }
+}
+
+function showPreviousGames() {
+  $('#games').empty()
+  $.get('/games', savedGames => {
+    if(savedGames.data.length) {
+      savedGames.data.forEach(function(game){
+        $('#games').append(`<button id="gameid-${game.id}">${game.id}</button><br />`)
+        $(`#gameid-${game.id}`).on('click', () => resetGame(game.id))
+      })
+    }
+  })
+}
+
+function attachListeners(){
+  $('td').on('click', function(){
+    if(!$.text(this) && !checkWinner()) {
+      doTurn(this)
+    }
+  })
+
+  $('#save').on('click', () => saveGame())
+  $('#previous').on('click', () => showPreviousGames())
+  $('#clear').on('click', () => resetBoard())
+}
